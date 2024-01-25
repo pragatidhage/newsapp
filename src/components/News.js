f2904348476426298f526713a6a21cc99b3d78ea
Pragati Dhage <pragatidhage971@gmail.com>
2024-01-25 20:18:01 +0530

    added card from bootstrap and structured the news page

--- src/components/News.js
+++ src/components/News.js
@@ -4,12 +4,21 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        this is news component
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2>NewSapp - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title='myTitle' description='myDesc'/>
+          </div>
+          <div className="col-md-4">
+            <NewsItem title='myTitle' description='myDesc'/>
+          </div>
+          <div className="col-md-4">
+            <NewsItem title='myTitle' description='myDesc'/>
+          </div>
+        </div>
+        
+        
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    //destructuring props will load and will return title and description
+    let {title,description} = this.props;
     return (
       <div>
-        this is news item
+        <div className="card" style={{width: '18rem'}}>
+          <img src="..." className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
       </div>
     )
   }
