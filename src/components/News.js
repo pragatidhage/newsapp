0dc14e38aa98d555bcb945376fdc396040823d91
Pragati Dhage <pragatidhage971@gmail.com>
2024-01-27 00:25:21 +0530

    used props to set pagesize && loading spinner is set

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
 
 export class News extends Component {
 
@@ -13,32 +14,41 @@ export class News extends Component {
     }
   }
   async componentDidMount(){
-    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=c34f3bc03e3448ebaa62a53f3a40e89f&page=1&pageSize=20";
+    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=c34f3bc03e3448ebaa62a53f3a40e89f&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true})
     let data= await fetch(url)
     let parsedData = await data.json()
-    this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading : false
+    })
   }
 
   handlePrevClick = async () =>{
     console.log("previous")
-    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=c34f3bc03e3448ebaa62a53f3a40e89f&page=${this.state.page - 1}&pageSize=20`;
+    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=c34f3bc03e3448ebaa62a53f3a40e89f&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true})
     let data= await fetch(url)
     let parsedData = await data.json()
     this.setState({
       page : this.state.page - 1,
-      articles: parsedData.articles
+      articles: parsedData.articles,
+      loading : false
     })
   }
 
   handleNextClick = async () =>{
     console.log("next")
-    if(this.state.page+1 <= Math.ceil(this.state.totalResults/20)){
-      let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=c34f3bc03e3448ebaa62a53f3a40e89f&page=${this.state.page + 1}&pageSize=20`;
+    if(this.state.page+1 <= Math.ceil(this.state.totalResults/this.props.pageSize)){
+      let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=c34f3bc03e3448ebaa62a53f3a40e89f&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      this.setState({loading: true})
       let data= await fetch(url)
       let parsedData = await data.json()
       this.setState({
         page : this.state.page + 1,
-        articles: parsedData.articles
+        articles: parsedData.articles,
+        loading : false
       })
     }  
   }
@@ -46,9 +56,10 @@ export class News extends Component {
   render() {
     return (
       <div className='container my-3'>
-        <h1>NewSapp - Top Headlines</h1>
+        <h1 className='text-center'>NewSapp - Top Headlines</h1>
+       { this.state.loading && <Spinner/>}
         <div className="row">
-          {this.state.articles.map((element)=>{
+          {!this.state.loading && this.state.articles.map((element)=>{
             return  <div className="col-md-4" key={element.url}>
             <NewsItem  title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imgUrl={element.urlToImage} newsUrl={element.url}/>
             </div>
@@ -56,7 +67,7 @@ export class News extends Component {
         </div> 
         <div className='container d-flex justify-content-between'>
           <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-          <button disabled={(this.state.page+1 > Math.ceil(this.state.totalResults/20))} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+          <button disabled={(this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize))} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>  
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,7 +9,7 @@ export class NewsItem extends Component {
       <div className='my-3'>
         <div className="card" style={{width: '18rem'}}>
           <img src={!imgUrl?"https://www.livemint.com/lm-img/img/2024/01/25/1600x900/narendra_modi_1706149904523_1706149904988.jpg":imgUrl} 
-          style={{height:"165px",width:"287px"}} className="card-img-top" alt="..."/>
+          style={{height:"165px",width:"287px",objectFit:"cover"}} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
