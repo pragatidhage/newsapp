8664977852114ea688951f9d9bb0b45bf8fb33f5
Pragati Dhage <pragatidhage971@gmail.com>
2024-01-29 01:31:29 +0530

    hidded api key with the help of .env.local

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component {
   pageSize = 12;
-
+  apiKey = process.env.REACT_APP_NEWS_API
   state = {
     progress : 0
   }
@@ -32,7 +32,7 @@ export default class App extends Component {
               exact
               path="/"
               element={
-                <News setProgress={this.setProgress} 
+                <News setProgress={this.setProgress} apiKey={this.apiKey} 
                   key="general"
                   pageSize={this.pageSize}
                   country="in"
@@ -44,7 +44,7 @@ export default class App extends Component {
               exact
               path="/business"
               element={
-                <News setProgress={this.setProgress} 
+                <News setProgress={this.setProgress} apiKey={this.apiKey} 
                   key="business"
                   pageSize={this.pageSize}
                   country="in"
@@ -56,7 +56,7 @@ export default class App extends Component {
               exact
               path="/entertainment"
               element={
-                <News setProgress={this.setProgress} 
+                <News setProgress={this.setProgress} apiKey={this.apiKey} 
                   key="entertainment"
                   pageSize={this.pageSize}
                   country="in"
@@ -68,7 +68,7 @@ export default class App extends Component {
               exact
               path="/general"
               element={
-                <News setProgress={this.setProgress} 
+                <News setProgress={this.setProgress} apiKey={this.apiKey} 
                   key="general"
                   pageSize={this.pageSize}
                   country="in"
@@ -80,7 +80,7 @@ export default class App extends Component {
               exact
               path="/health"
               element={
-                <News setProgress={this.setProgress} 
+                <News setProgress={this.setProgress} apiKey={this.apiKey} 
                   key="health"
                   pageSize={this.pageSize}
                   country="in"
@@ -92,7 +92,7 @@ export default class App extends Component {
               exact
               path="/science"
               element={
-                <News setProgress={this.setProgress} 
+                <News setProgress={this.setProgress} apiKey={this.apiKey} 
                   key="science"
                   pageSize={this.pageSize}
                   country="in"
@@ -104,7 +104,7 @@ export default class App extends Component {
               exact
               path="/sports"
               element={
-                <News setProgress={this.setProgress} 
+                <News setProgress={this.setProgress} apiKey={this.apiKey} 
                   key="sports"
                   pageSize={this.pageSize}
                   country="in"
@@ -116,7 +116,7 @@ export default class App extends Component {
               exact
               path="/technology"
               element={
-                <News setProgress={this.setProgress} 
+                <News setProgress={this.setProgress} apiKey={this.apiKey} 
                   key="technology"
                   pageSize={this.pageSize}
                   country="in"
