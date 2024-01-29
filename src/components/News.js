bde78dcb87cfd6c734e04fd3aee5b9848055b3ac
Pragati Dhage <pragatidhage971@gmail.com>
2024-01-29 11:50:57 +0530

    classbased component to function based component except app.js

--- src/components/News.js
+++ src/components/News.js
@@ -1,94 +1,97 @@
-import React, { Component } from "react";
+import React , {useEffect,useState} from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 
-export class News extends Component {
-  static defaultProps = {
-    country: "in",
-    pageSize: 6,
-    category: "general",
-  };
+const News = (props)=>{
+ 
+  const[articles,setArticles]=useState([])
+  const[loading,setLoading]=useState(true)
+  const[page,setPage]=useState(1)
+  const[totalResults,setTotalResults]=useState(0)
 
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  };
-
-  capitalizeFirstLetter = (string) => {
+  const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
-  };
+  };  
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      loading: false,
-      page: 1,
-      totalResults : 0
-    };
-    document.title = `NewSapp- Top ${this.capitalizeFirstLetter(
-      this.props.category
-    )} Headlines`;
-  }
+  document.title = `NewSapp- Top ${capitalizeFirstLetter(
+    props.category
+  )} Headlines`;
 
-  async updateNews() {
-    this.props.setProgress(10)
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
+  
+
+
+ 
+
+  const updateNews= async ()=> {
+    props.setProgress(10)
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    
+    setLoading(true)
     let data = await fetch(url);
     
     let parsedData = await data.json();
+    
+    setArticles(parsedData.articles)
+    setTotalResults(parsedData.totalResults)
+    setLoading(false)
    
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false,
-    });
    
-    this.props.setProgress(100)
+    props.setProgress(100)
   }
+  
+  useEffect(()=>{
 
-  async componentDidMount() {
-    this.updateNews();
-  }
+  updateNews();
+
+  },[ ] )
+
+  
+  // const handlePrevClick = async ()=>{
+   
+  //   setPage(page-1);
+  //  updateNews()
+  // }
+
+  // const handleNextClick = async ()=>{
+   
+  //   setPage(page+1);
+  //  updateNews()
+  // }
 
 
-
-  fetchMoreData= async ()=>{
-    this.setState({page : this.state.page + 1})
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+  const fetchMoreData= async ()=>{
+    setPage(page+1);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({
-      articles: this.state.articles.concat(parsedData.articles),
-      totalResults: parsedData.totalResults,
-    });
+    
+    setArticles(articles.concat(parsedData.articles))
+    setTotalResults(parsedData.totalResults)
   }
 
-  render() {
+  
     return (
       <>
         <strong>
           <h1 className="text-center" style={{ margin: "25px 0px" }}>
-            {`NewSapp - Top  ${this.capitalizeFirstLetter(
-      this.props.category
+            {`NewSapp - Top  ${capitalizeFirstLetter(
+      props.category
     )} Headlines`}
           </h1>
         </strong>
-        {this.state.loading && <Spinner />}
+        {loading && <Spinner />}
         <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.totalResults}
+          dataLength={articles.length}
+          next={fetchMoreData}
+          hasMore={articles.length !== totalResults}
           loader={<Spinner />}
         >
         <div className="container">
           <div className="row">
-            {this.state.articles.map((element) => {
+            {articles.map((element) => {
               return (
                 <div className="col-md-4" key={element.url}>
                   <NewsItem
@@ -114,6 +117,19 @@ export class News extends Component {
       </>
     );
   }
-}
+  
+
+
+News.defaultProps = {
+  country: "in",
+  pageSize: 6,
+  category: "general",
+};
+
+ News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
 
 export default News;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,55 +1,51 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    //destructuring props will load and will return title and description
-    let { title, description, imgUrl, newsUrl, author, date, source } =
-      this.props;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              position: "absolute",
-              right: "0",
-            }}
+const NewsItem = (props) => {
+  //destructuring props will load and will return title and description
+  let { title, description, imgUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span className="badge rounded-pill bg-danger">{source}</span>
+        </div>
+
+        <img
+          src={
+            !imgUrl
+              ? "https://www.livemint.com/lm-img/img/2024/01/25/1600x900/narendra_modi_1706149904523_1706149904988.jpg"
+              : imgUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              By {author ? author : "Unknown"} on {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blank"
+            className="btn btn-sm btn-dark"
           >
-            <span className="badge rounded-pill bg-danger">{source}</span>
-          </div>
-
-          <img
-            src={
-              !imgUrl
-                ? "https://www.livemint.com/lm-img/img/2024/01/25/1600x900/narendra_modi_1706149904523_1706149904988.jpg"
-                : imgUrl
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <p className="card-text">
-              <small className="text-body-secondary">
-                By {author ? author : "Unknown"} on{" "}
-                {new Date(date).toGMTString()}
-              </small>
-            </p>
-            <a
-              rel="noreferrer"
-              href={newsUrl}
-              target="_blank"
-              className="btn btn-sm btn-dark"
-            >
-              Read More
-            </a>
-          </div>
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsItem;
